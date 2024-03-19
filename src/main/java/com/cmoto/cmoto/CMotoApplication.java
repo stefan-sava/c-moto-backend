77d97d886ac51bfee6db2615d38eb2928fc5282c
stefan-sava <77351630+stefan-sava@users.noreply.github.com>
2024-03-19 16:03:32 +0200

    Add files via upload

---
+++ src/main/java/com/cmoto/cmoto/CMotoApplication.java
@@ -0,0 +1,13 @@
+package com.cmoto.cmoto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CMotoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CMotoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cmoto/cmoto/CMotoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cmoto.cmoto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CMotoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
